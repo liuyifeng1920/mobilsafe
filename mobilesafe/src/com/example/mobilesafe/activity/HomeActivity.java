e4a1b4d93125ba379a697f2850ee17783f7b9a2e
liuyfieng1920 <woaini789>
2016-07-11 18:18:29 +0800

    7.11

---
+++ mobilesafe/src/com/example/mobilesafe/activity/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.example.mobilesafe.activity;
+
+import com.example.mobilesafe.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Administrator
+ * Ê×Ò³
+ */
+public class HomeActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
+}
