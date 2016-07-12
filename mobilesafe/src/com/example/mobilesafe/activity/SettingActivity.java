5d38db4ec82cd4f85c9330eb6a86c466b5affc91
liuyfieng1920 <woaini789>
2016-07-12 17:22:18 +0800

    7.12.1

---
+++ mobilesafe/src/com/example/mobilesafe/activity/SettingActivity.java
@@ -0,0 +1,21 @@
+package com.example.mobilesafe.activity;
+
+import com.example.mobilesafe.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingActivity extends Activity {
+
+	public SettingActivity() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_setting);
+	}
+}
